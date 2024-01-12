49c723f8912b1d23d29da9ccc28bde9fe49e44a5
Binayak Koirala <108163048+BinayakKoirala@users.noreply.github.com>
2024-01-12 11:08:25 +0545

    Add files via upload

---
+++ ingest.py
@@ -0,0 +1,26 @@
+import os
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.embeddings import HuggingFaceBgeEmbeddings
+from langchain.document_loaders import DirectoryLoader
+from langchain.document_loaders import PyPDFLoader
+
+model_name = "BAAI/bge-large-en"
+model_kwargs = {'device': 'cpu'}
+encode_kwargs = {'normalize_embeddings': False}
+embeddings = HuggingFaceBgeEmbeddings(
+    model_name=model_name,
+    model_kwargs=model_kwargs,
+    encode_kwargs=encode_kwargs
+)
+
+
+
+loader= PyPDFLoader("C:\\Users\Binayak K\\Desktop\\Random projects\\nepal kanun\\nepallife.pdf")
+documents = loader.load()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+texts = text_splitter.split_documents(documents)
+
+vector_store = Chroma.from_documents(texts, embeddings, collection_metadata={"hnsw:space": "cosine"}, persist_directory="stored/nepdata")
+
+print("Vector Store Created.......")
